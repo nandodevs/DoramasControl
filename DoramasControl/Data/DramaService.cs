ba4cbde1046acdcfd16802e844751fa481a90e70
nandodevs <nando.devs@gmail.com>
2024-04-01 17:29:04 -0300

    Edit Series View

--- DoramasControl/Data/DramaService.cs
+++ DoramasControl/Data/DramaService.cs
@@ -1,20 +1,38 @@
-﻿using DoramasControl.Models;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using DoramasControl.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DoramasControl.Data
 {
     public class DramaService
     {
         private readonly HttpClient _httpClient;
+        private readonly IMemoryCache _cache;
 
-        public DramaService(HttpClient httpClient)
+        public DramaService(HttpClient httpClient, IMemoryCache cache)
         {
             _httpClient = httpClient;
+            _cache = cache;
         }
 
         public async Task<DramaViewModel[]> GetTopDramas()
         {
-            //Retorno da API no Vercel
-            return await _httpClient.GetFromJsonAsync<DramaViewModel[]>(requestUri: "https://mdl-flask-api.onrender.com/api/doramas-stars");
+            // Verifica se os dados já estão em cache
+            if (!_cache.TryGetValue("topDramas", out DramaViewModel[] topDramas))
+            {
+                // Se não estiver em cache, faz a consulta à API
+                var response = await _httpClient.GetAsync("https://mdl-flask-api.onrender.com/api/doramas-stars");
+                response.EnsureSuccessStatusCode();
+                topDramas = await response.Content.ReadFromJsonAsync<DramaViewModel[]>();
+
+                // Armazena os dados em cache por 10 minutos
+                _cache.Set("topDramas", topDramas, TimeSpan.FromMinutes(10));
+            }
+
+            return topDramas;
         }
     }
 }
